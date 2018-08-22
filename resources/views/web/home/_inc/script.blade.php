af8ef1eefe30bfab3f1ddbb2be511eca2e1e37cf
Justin Ajith <justin.ceymplon@jeylabs.com.au>
2018-08-22 20:35:58 +0530

    Add api route (Error)

--- resources/views/web/home/_inc/script.blade.php
+++ resources/views/web/home/_inc/script.blade.php
@@ -1,5 +1,8 @@
 <script>
     app.controller('homeController', function($http, $scope){
-
+        $scope.baseUrl = '{{ URL::to('api/web-home') }}';
+        $http.get($scope.baseUrl + '/new-products').then(function(response){
+            $scope.newProducts = response.data;
+        });
     });
 </script>
--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,11 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['prefix'=>'api', 'namespace'=>'Web'], function($routes){
+    $routes->group(['prefix'=>'web-home', 'namespace'=>'Home'], function($routes){
+        $routes->get('/new-products', 'HomeController@newProduct');
+    });
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
